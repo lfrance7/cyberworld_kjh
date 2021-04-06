54bb78d15b82d9966a2c6c3a1751cc29c43439bd
lfrance7@naver.com <77479700+lfrance7@users.noreply.github.com>
2021-04-06 20:05:15 +0900

    환경세팅 완료

---
+++ src/main/java/com/cos/blog/test/BlogControllerTest.java
@@ -0,0 +1,16 @@
+package com.cos.blog.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class BlogControllerTest {
+
+	@GetMapping("/kjh/hello")
+	public String  hello() {
+		
+		return "<h1>hello spring boot</h1>";
+	}
+	
+}
